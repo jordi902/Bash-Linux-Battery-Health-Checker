3602af380b3136c2f426c0da113b54eab406a975
jordi902 <163332476+jordi902@users.noreply.github.com>
2024-03-13 17:58:42 +0100

    My first commit

---
+++ Bash-Linux-Battery-Health-Checker.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+noColor="\e[0m"
+yellow="\e[0;33m"
+red="\e[0;31m"
+green="\e[0;32m"
+
+test_dir="/sys/class/power_supply/BAT*"
+test_upower=$(which upower &> /dev/null)
+test_acpi=$(which acpi &> /dev/null)
+
+dirFunction () {
+	fullDesign=$(cat /sys/class/power_supply/BAT*/charge_full_design)
+	fullCurrent=$(cat /sys/class/power_supply/BAT*/charge_full)
+	cycleCount=$(cat /sys/class/power_supply/BAT*/cycle_count)
+	capacity=$(( $fullCurrent * 100 / $fullDesign ))
+	echo -e "The health of your battery is: ${green}"$capacity"${noColor}"
+	echo -e "The cycles charge on your battery are: ${green}"$cycleCount"${noColor}"
+}
+
+upowerFunction () {
+	battery=$(upower --enumerate | grep battery_BAT*)
+	if [[ ! $battery ]]; then
+		echo -e "${red}[!]${noColor}An error ocurred while selecting the battery"
+		exit 1
+	fi
+
+	info=$(upower -i $battery | grep -E 'capacity|cycles')
+	if [[ ! $info ]]; then
+		echo -e "${red}[!]${noColor}An error ocurred while selecting the data fields"
+	fi
+
+	echo -e "The health of your battery is: ${green}"$(echo $info | awk '{print $4}')"${noColor}"
+	echo -e "The cycles charge on your battery are: ${green}"$(echo $info | awk '{print $2}')"${noColor}"
+}
+
+acpiFunction () {
+	infoAcpi=$(acpi -bi | grep %)
+	if [[ ! $infoAcpi ]]; then
+		echo -e "${red}[!]${noColor}An error ocurred while selecting the battery info"
+		exit 1
+	fi
+	echo $infoAcpi | awk '{print "The health of your battery is: ""'$(echo -e "${green}")'"$NF"'$(echo -e "${noColor}")'"}'
+}
+
+if [[ ! -d $test_dir ]]; then
+	dirFunction
+	exit
+elif [[ ! $test_upower ]]; then
+        upowerFunction
+        exit
+elif [[ ! $test_acpi ]]; then
+        acpiFunction
+        exit
+else
+        echo -e "${red}[!]${noColor}There was an error running the script"
+        exit 1
+fi
